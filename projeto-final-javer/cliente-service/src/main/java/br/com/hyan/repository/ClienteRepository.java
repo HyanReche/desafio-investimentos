a4f34b02180c847bc3700617ec4b9360574e86e8
HyanReche <hyan.reche@gmail.com>
2025-02-05 09:23:28 -0300

    Delete projeto-final-javer directory

--- projeto-final-javer/cliente-service/src/main/java/br/com/hyan/repository/ClienteRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.hyan.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.hyan.model.Cliente;
-
-public interface ClienteRepository extends JpaRepository<Cliente, Long>{
-
-}
